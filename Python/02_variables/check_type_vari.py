5e890efe1938bac70410f2c2dac7b2b2b9e937bb
Kcodev <odkhoder@gmail.com>
2022-07-29 17:13:21 +0300

    added variables

---
+++ Python/02_variables/check_type_vari.py
@@ -0,0 +1,15 @@
+# Checking the type of a variable
+f = 45
+
+# use a function type
+print(type(f))
+
+# checking for decimal numbers
+z = 40.1
+
+print(type(z))
+
+# for characters
+
+name = "Sammy Kipkosgei"
+print(type(name))
---
+++ Python/02_variables/main.py
@@ -0,0 +1,16 @@
+# varible is storages of things numbers, strings,etc
+age = 21
+
+print(age)
+first_name = "Sammy"
+print(first_name)
+
+# stroting numbers
+num_1 = 12
+num_2 = 13
+
+# print(num_1 + num_2)
+
+results = num_1 + num_2
+
+print(results)
